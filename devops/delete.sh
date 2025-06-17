a9f8e0f4674359f31dde44484dcd2d4b5fc37e6c
sach <s@gmail.com>
2025-06-17 05:51:13 +0530

    cleaned files

--- devops/delete.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl -n argocd delete -f argocd-manifest.yaml
-kubectl -n argocd delete -f argocd-ingress.yaml
-kubectl delete ns argocd
--- devops/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-[ kubectl get ns argocd ] &> /dev/null || kubectl create ns argocd
-kubectl -n argocd apply -f argocd-manifest.yaml
-kubectl -n argocd apply -f argocd-ingress.yaml
